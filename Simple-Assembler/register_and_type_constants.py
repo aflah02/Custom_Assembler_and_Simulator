1bef5f5b4f0e22e13956cc3f3c87271e60aed538
Aflah <aflahkhan.2020@gmail.com>
2021-08-08 16:51:07 +0530

    Added Lookup Dicts for accessing type info and register encodings

--- Simple-Assembler/register_and_type_constants.py
+++ Simple-Assembler/register_and_type_constants.py
@@ -6,3 +6,37 @@ type_to_reg_no = {
     'E':0,
     'F':0
 }
+type_to_imm_no = {
+    'A':0,
+    'B':1,
+    'C':0,
+    'D':0,
+    'E':0,
+    'F':0
+}
+type_to_unusedbits = {
+    'A':2,
+    'B':0,
+    'C':5,
+    'D':0,
+    'E':3,
+    'F':11
+}
+type_to_memoryaddress = {
+    'A':0,
+    'B':0,
+    'C':0,
+    'D':1,
+    'E':1,
+    'F':0
+}
+register_to_encoding = {
+    'RO': '000',
+    'R1': '001',
+    'R2': '010',
+    'R3': '011',
+    'R4': '100',
+    'R5': '101',
+    'R6': '110',
+    'FLAGS': '111',
+}
