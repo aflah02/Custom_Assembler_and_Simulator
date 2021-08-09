b3041c71a42916a6c391561b46aed95636fd72cf
fruitkijin <82924774+fruitkijin@users.noreply.github.com>
2021-08-09 16:21:13 +0530

    Update helper_functions.py

--- Simple-Assembler/helper_functions.py
+++ Simple-Assembler/helper_functions.py
@@ -27,34 +27,51 @@ def isVarValid(line_comp, count, alphanum):
             else:
                 return -3
             
-
-def isLineValid(line):
+def isLabelValid(line_comp,alphanum):
+    a = line_comp[0]
+    counter = 0
+    b = len(a) - 1
+    if a[-1::] = ":":
+        for i in a:
+            if i in aplhanum:
+                counter+=1
+        if counter == b:
+            
+                
+        else:
+            return -1 
+        
+def isLineValid(line_comp):
     """Checks if line is valid that is the instruction is valid and the size corresponds to the instruction"""
-    ls_input = list(map(str, line.split()))
-    if isInstructionValid(ls_input[0]) == False:
+    if isInstructionValid(line_comp[0]) == False:
         return -1
-    if isSizeRight(ls_input[0], ls_input) == False:
+    if isSizeRight(line_comp[0], line_comp) == False:
         return -2
     else:
         return 1
 
 
-def lineTypesMatch(line):
+def lineTypesMatch(line_comp):
     """Checks if the objects in the line match the objects which they were supposed to be i.e. registers
     are in place of registers in the syntax and so on"""
-    ls_input = list(map(str, line.split()))
-    ls_type_order = type_to_syntaxconstituents[OPcode_table[ls_input[0]][-1]]
-    for i in range(1, len(ls_input)):
+    ls_type_order = type_to_syntaxconstituents[OPcode_table[line_comp[0]][-1]]
+    for i in range(1, len(line_comp)):
         if ls_type_order[i] == 'Register':
-            if isRegisterValid(ls_input[i]) is False:
+            if isRegisterValid(line_comp[i]) is False:
                 return -1
-            if isRegisterValid(ls_input[i])==-1:
+            if isRegisterValid(line_comp[i])==-1:
                 return -4
-        elif ls_type_order[i] == 'Immediate':
-            if isImmediateValid(ls_input[i]) is False:
+        if ls_type_order[i] == 'Immediate':
+            if isImmediateValid(line_comp[i]) is False:
                 return -2
-            if isImmediateRangeValid(ls_input[i]) is False:
+            if isImmediateRangeValid(line_comp[i]) is False:
                 return -3
-        else:
-            pass
+        if ls_type_order[i] == 'Memory Address':
+            if line_comp[0]=='ld' or line_comp[0]=='st':
+                if line_comp[-1] in var_declared:
+            if line_comp[0]=='jmp' or line_comp[0]=='jlt' or line_comp[0]=='jgt' or line_comp[0]=='je': 
+                if line_comp[-1] in lbl_declared:
+                else
+            else:
+                return -5
     return 0
