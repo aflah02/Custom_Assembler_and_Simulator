b26ec574af9b90ba152d7ef89a57a1b09326f70f
fruitkijin <82924774+fruitkijin@users.noreply.github.com>
2021-08-09 13:04:08 +0530

    Update general_consts.py

--- Simple-Assembler/general_consts.py
+++ Simple-Assembler/general_consts.py
@@ -5,3 +5,14 @@ immediate_size = 8
 ls_instructions = ['add','sub','mov','ld','st','mul','div','rs','ls','xor','or',
 'and','not','cmp','jmp','jlt','jgt','je','hlt']
 ls_registers = ['R0','R1','R2','R3','R4','R5','R6','FLAGS']
+alphanum = []
+for k in range(65,91):
+    b = chr(k)
+    alphanum.append(b)
+for k in range(97,123):
+    b = chr(k)
+    alphanum.append(b)
+for k in range(48,58):
+    b = chr(k)
+    alphanum.append(b)
+alphanum.append(chr(95))
