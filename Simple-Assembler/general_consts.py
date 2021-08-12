15108ab5ae794e80e332959ae34dd8e7830e992a
Divyam Gupta <82924774+fruitkijin@users.noreply.github.com>
2021-08-12 18:24:31 +0530

    Update general_consts.py

--- Simple-Assembler/general_consts.py
+++ Simple-Assembler/general_consts.py
@@ -4,6 +4,8 @@ memory_address_size = 8
 immediate_size = 8
 ls_instructions = ['add','sub','movi','movr','ld','st','mul','div','rs','ls','xor','or',
 'and','not','cmp','jmp','jlt','jgt','je','hlt']
+ls_instructions2 = ['add','sub','mov','ld','st','mul','div','rs','ls','xor','or',
+'and','not','cmp','jmp','jlt','jgt','je','hlt']
 ls_registers = ['R0','R1','R2','R3','R4','R5','R6','FLAGS']
 alphanum = []
 for k in range(65,91):
