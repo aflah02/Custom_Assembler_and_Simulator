12856e20184301cfee4a2dab7f8fc48ea3c80a1c
aflah02 <aflahkhan.2020@gmail.com>
2021-08-13 04:15:23 +0530

    Added Documentation for Parsers

--- Simple-Assembler/parsers.py
+++ Simple-Assembler/parsers.py
@@ -3,15 +3,19 @@ from register_and_type_constants import *
 
 
 def opcode_parser(instruction):
+    """Parses OPCODE for Given Instruction"""
     return OPcode_table[instruction][0]
 
 
 def immediate_parser(immediate):
+    """Parses Binary Value for Given Immediate"""
     imm = int(immediate[1:])
     return '{0:08b}'.format(imm)
 
 def memory_address_parser(location):
+    """Parses Binary Value for Given Memory Address"""
     return '{0:08b}'.format(location)
 
 def register_parser(register):
+    """Parses Register Encoding for Given Register Name"""
     return register_to_encoding[register]
