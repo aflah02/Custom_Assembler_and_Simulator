070110b8fccca6173105c62da241c25b69f48ce8
aflah02 <aflahkhan.2020@gmail.com>
2021-08-15 01:17:08 +0530

    New Journey Begins

---
+++ SimpleSimulator/simulator.py
@@ -0,0 +1,26 @@
+register_tracker = {
+    'R0': 0,
+    'R1': 0,
+    'R2': 0,
+    'R3': 0,
+    'R4': 0,
+    'R5': 0,
+    'R6': 0,
+}
+
+flags = {
+    'V':0,
+    'L':0,
+    'E':0
+}
+
+halt_encountered = False
+
+ls_inputs = []
+while True:
+    try:
+        input_line = input()
+        ls_inputs.append(input_line)
+    except EOFError:
+        break
+print(ls_inputs)
