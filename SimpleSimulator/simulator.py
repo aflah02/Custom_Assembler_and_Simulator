fba9bb78c0e781592add1d7a4c23cdca7e95f8f9
Stealth-py <generalom1234@gmail.com>
2021-08-15 16:53:52 +0530

    type c executor completed

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -43,12 +43,30 @@ def type_a_executor(instruction, second_register, third_register):
         return register_tracker[second_register] ^ register_tracker[third_register]
 
 def type_b_executor(instruction, first_register, immediate):
-    if instruction == 'mov':
+    if instruction == 'movi':
         return immediate
     elif instruction == 'rs':
         return register_tracker[first_register] >> immediate
     elif instruction == 'ls':
         return register_tracker[first_register] << immediate
+
+def type_c_executor(instruction, first_register, second_register):
+    if instruction == 'movr':
+        return second_register
+    elif instruction == 'div':
+        a, b = register_tracker[first_register], register_tracker[second_register]
+        return (a//b, a%b)
+    elif instruction == 'not':
+        return ~(register_tracker[second_register])
+    elif instruction == 'cmp':
+        a, b = register_tracker[first_register], register_tracker[second_register]
+        if a==b:
+            return "E"
+        elif a<b:
+            return "L"
+        else:
+            return "G"
+    pass
 
 ls_inputs = []
 while True:
@@ -85,9 +103,18 @@ while(halt_encountered == False):
                 register_tracker[register_1] = value_to_store
         else:
             register_tracker[register_1] = value_to_store
-    if instruction_type == 'B':
+    elif instruction_type == 'B':
         register = encoding_to_register[component_list[1]]
         immediate = immediate_parser(component_list[-1])
         value_to_store = type_b_executor(instruction, register, immediate)
         register_tracker[register] = value_to_store
-        
+    elif instruction_type == 'C':
+        first_register = encoding_to_register[component_list[1]]
+        second_register = encoding_to_register[component_list[-1]]
+        value_to_store = type_c_executor(instruction, first_register, second_register)
+        if instruction == 'movr' or instruction == 'not':
+            register_tracker[first_register] = value_to_store
+        elif instruction == 'div':
+            register_tracker[first_register], register_tracker[second_register] = value_to_store
+        elif instruction == 'cmp':
+            flags[value_to_store] = 1
