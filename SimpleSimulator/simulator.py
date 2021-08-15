fc4121b8f7e6669e948ba91547100ee1fb7275a6
aflah02 <aflahkhan.2020@gmail.com>
2021-08-15 10:40:17 +0530

    Added New Parsers and Helper Functions, Fixed Main

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -21,19 +21,33 @@ labels = {}
 halt_encountered = False
 
 def type_a_executor(instruction, second_register, third_register):
-    if (instruction == 'add'):
+    if instruction == 'add':
         return register_tracker[second_register] + register_tracker[third_register]
-    elif (instruction == 'sub'):
+    elif instruction == 'sub':
         return register_tracker[second_register] - register_tracker[third_register]
-    pass
+    elif instruction == 'mul':
+        return register_tracker[second_register] * register_tracker[third_register]
+    elif instruction == 'xor':
+        return register_tracker[second_register] ^ register_tracker[third_register]
+    elif instruction == 'or':
+        return register_tracker[second_register] | register_tracker[third_register]
+    elif instruction == 'and':
+        return register_tracker[second_register] & register_tracker[third_register]
+    elif instruction == 'xor':
+        return register_tracker[second_register] ^ register_tracker[third_register]
 
-ls_inputs = []
-while True:
-    try:
-        input_line = input()
-        ls_inputs.append(input_line)
-    except EOFError:
-        break
 
-while(halt_encountered == False):
-    pass
+# ls_inputs = []
+# while True:
+#     try:
+#         input_line = input()
+#         ls_inputs.append(input_line)
+#     except EOFError:
+#         break
+
+# while(halt_encountered == False):
+#     pass
+
+register_tracker['R0'] = 10
+register_tracker['R3'] = 20
+print(type_a_executor('add', 'R0', 'R3'))
